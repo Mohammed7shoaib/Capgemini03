614f486d21261b079aa2d26443a59de5cd7b7b99
tnsda <tnsda@TNS_DA14>
2026-08-19 10:56:49 +0530

    Addition

---
+++ Corejava/src/com/tns/packagesprogram/Demoprogram.java
@@ -0,0 +1,9 @@
+package com.tns.packagesprogram;
+
+public class Demoprogram {
+public int a = 90;
+public String s="Mohammed";
+public static void main(String[] args) {
+	System.out.println();
+}
+}
