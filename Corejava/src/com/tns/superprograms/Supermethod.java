d184e730fed9cc0897c292362bd169c9ef0013bf
tnsda <tnsda@TNS_DA14>
2026-08-25 14:23:53 +0530

    .

---
+++ Corejava/src/com/tns/superprograms/Supermethod.java
@@ -0,0 +1,19 @@
+package com.tns.superprograms;
+
+class Employe1{
+	void calculatePerformance() {
+		System.out.println("Calculating employee performance");
+	}
+}
+
+class Senioremployee extends Employe1{
+	super.calculatePerformance() {
+		System.out.println("Calculating leadership performance");
+	}
+}
+
+public class Supermethod {
+public static void main(String[] args) {
+	
+}
+}
