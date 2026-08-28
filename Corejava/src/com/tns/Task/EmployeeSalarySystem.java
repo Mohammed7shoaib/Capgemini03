692cab56fba225947ae00ac320a648f44d6cc624
tnsda <tnsda@TNS_DA14>
2026-08-28 10:25:53 +0530

    Task

---
+++ Corejava/src/com/tns/Task/EmployeeSalarySystem.java
@@ -0,0 +1,40 @@
+package com.tns.Task;
+
+abstract class Employee {
+
+    abstract void calculateSalary();
+}
+
+class FullTimeEmployee extends Employee {
+
+    double monthlySalary = 50000;
+
+    @Override
+    void calculateSalary() {
+        System.out.println("Full Time Employee Salary: " + monthlySalary);
+    }
+}
+
+class PartTimeEmployee extends Employee {
+
+    int hoursWorked = 80;
+    double hourlyRate = 500;
+
+    @Override
+    void calculateSalary() {
+        double salary = hoursWorked * hourlyRate;
+        System.out.println("Part Time Employee Salary: " + salary);
+    }
+}
+
+public class EmployeeSalarySystem {
+
+    public static void main(String[] args) {
+
+        FullTimeEmployee f = new FullTimeEmployee();
+        PartTimeEmployee p = new PartTimeEmployee();
+
+        f.calculateSalary();
+        p.calculateSalary();
+    }
+}
